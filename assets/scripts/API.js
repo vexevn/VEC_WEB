e931295f565f224775669a305e11f20bc623f739
duytrung93 <32829022+duytrung93@users.noreply.github.com>
2024-10-15 11:38:43 +0700

    Update API.js

--- assets/scripts/API.js
+++ assets/scripts/API.js
@@ -13,7 +13,7 @@ if (typeof location != "undefined") {
   else linkAPI = location.protocol + "//" + location.hostname + ":8114/";
 }
 
-linkAPI = "http://demo.quanlynoibo.com:8114/";
+// linkAPI = "http://demo.quanlynoibo.com:8114/";
 // linkAPI = "http://localhost:63008/";
 export var linkPurchase = "http://purchase.quanlynoibo.com:8060";
 export default {
